c8f6825f1826c78be88f8cdcd5e5ae6d8a955572
ari0111 <tbartek1@edu.cdv.pl>
2017-01-25 18:34:34 +0100

    Tomek
    Klient dodany

---
+++ Nowy projekt1/WindowsFormsApplication1/WindowsFormsApplication1/Klient_prywatny.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Klient_prywatny : Form
+    {
+        public Klient_prywatny()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
