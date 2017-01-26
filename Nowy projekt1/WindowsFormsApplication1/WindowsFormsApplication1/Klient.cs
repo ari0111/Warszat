af30f0c73a07292b3a5fd560607279c3f61d6dfe
ari0111 <tbartek1@edu.cdv.pl>
2017-01-26 13:59:01 +0100

    Zmiana

--- Nowy projekt1/WindowsFormsApplication1/WindowsFormsApplication1/Klient.cs
+++ Nowy projekt1/WindowsFormsApplication1/WindowsFormsApplication1/Klient.cs
@@ -32,5 +32,10 @@ namespace WindowsFormsApplication1
 
             }
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
