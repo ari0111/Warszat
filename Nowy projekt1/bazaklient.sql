c5ebe30290424ac629209f697a20e7536e9d7721
ari0111 <tbartek1@edu.cdv.pl>
2017-01-26 13:21:17 +0100

    baza
    aa

---
+++ Nowy projekt1/bazaklient.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[KlientId] INT NULL  IDENTITY, 
+    [Nazwa Firmy] NVARCHAR(50) NULL, 
+    [NIP] NVARCHAR(50) NULL, 
+    [Ulica] NVARCHAR(50) NULL, 
+    [Miasto] NVARCHAR(50) NULL, 
+    [Kod Pocztowy] NVARCHAR(50) NULL, 
+    [Telefon] NVARCHAR(50) NULL, 
+    [Email] NVARCHAR(50) NULL, 
+    [Imie] NVARCHAR(50) NULL, 
+    [Nazwisko] NVARCHAR(50) NULL, 
+    CONSTRAINT [PK_Table] PRIMARY KEY ([KlientId])
+)
