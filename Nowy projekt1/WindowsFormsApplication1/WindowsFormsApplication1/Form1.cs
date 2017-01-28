57e54eb1724c0a0473e9a24fe7e0fd4f19d0caf6
mxsysd <sdominiczak1@edu.cdv.pl>
2017-01-28 08:59:31 +0100

    logowanie
    logowanie edit

--- Nowy projekt1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ Nowy projekt1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -10,31 +10,22 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
+    
     public partial class Menu : Form
     {
         public Menu()
         {
             InitializeComponent();
         }
-
-        private void Menu_Load(object sender, EventArgs e)
-        {
-            this.Hide(); //ukrywasz form-e
-            logowanie fl = new logowanie();
-            fl.ShowInTaskbar = false;//nie pokazujesz na pasku zadań
-            fl.MaximizeBox = false; //nie ma ikonki maksymalizacji
-            fl.MinimizeBox = false; //nie ma ikonki minimalizacji
-            fl.ShowDialog();//wyświetlasz
-            string login = "login";
-            string haslo = "haslo";
-            
-           
-        }
-
         private void pictureBox1_Click(object sender, EventArgs e)
         {
             Klient form = new Klient();
             form.Show();
+        }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
